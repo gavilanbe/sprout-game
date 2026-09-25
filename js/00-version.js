a1a7a75927adc901f07d2caeae1b1878852a0a3e
Nahuel Gavilán <nahuelgavilanbe@gmail.com>
2026-09-25 15:16:07 +0200

    wip

--- js/00-version.js
+++ js/00-version.js
@@ -1,3 +1,3 @@
 'use strict';
 /* la versión del juego: la escribe scripts/version.cjs (fecha del último cambio y hash del contenido) */
-const GAME_VERSION='2026.09.25-0b42e2b9';
+const GAME_VERSION='2026.09.25-264a7447';
